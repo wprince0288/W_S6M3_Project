860050cecb97d89ee9cc2675936ca114e2a5989d
wprince0288 <wprince0288@gmail.com>
2024-07-12 14:22:55 -0400

    styled

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+border: 2.5px solid black;
+border-radius: 12px;
+padding: 1rem;
+
+h2 {
+    color: ${pr => pr.$color};
+    font-size: 2.5em;
+    margin: 0 0 1rem 0;
+}
+
+ p {
+    margin: 0 0 1.5rem 0;
+
+    &::first-line {
+      font-size: 1.5em;
+    }
+ }
+`
 
 export default function Card({ title, text, imageURL, date }) {
     return (
-        <div className='card'>
+        <StyledCard $color="royalblue"className='card'>
             <h2>{title}</h2>
             <p>{text}</p>
             <Figure
                 imageURL={imageURL}
                 caption={date}
             />
-        </div>
+        </StyledCard>
     )
 }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+max-width: 300px;
+
+figcaption {
+    font-style:italic;
+}
+
+`
 
 export default function Figure({ imageURL, caption }) {
     return (
